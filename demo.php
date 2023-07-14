4de86ad16a5d74e04ff4cda665b46e4d98ca2f17
Ngô Trọng Phong <ngotrongphong.dev@gmail.com>
2023-07-14 07:50:20 +0700

    gioi thieu

---
+++ demo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php echo "Hello world" ?>
+  <br>
+  <?php
+    $a = 2;
+    if($a < 10) {
+      echo "So $a nho hon 10" . ". Hieu chua hihi do ngoc";
+    } else {
+      echo "So $a lon hon 10";
+    }
+  ?>
+</body>
+</html>
